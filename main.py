ae480907096f8b7fa2bddc2f7921b9f392c65855
ctcomsk55 <76209097+ctcomsk55@users.noreply.github.com>
2024-11-21 20:43:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello')
+from tkinter import *
+def hello_world():
+        print("Hello world")
+
+
+# создаем окно main
+main = Tk()
+
+# добавляем кнопку, настраиваем ее форму и цвет
+# при нажатии сработает функция hello world 
+btn = Button(main, width=20,
+             height=7, text="Кнопка",
+             bg="white", fg="black",
+             command=hello_world)
+btn.pack()
+main.mainloop()
